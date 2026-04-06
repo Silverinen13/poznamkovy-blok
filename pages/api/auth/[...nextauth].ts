a26375d19e30e2405e26f5819aad3d62dd275622
Jan Stříbrský <stribja@student.uzlabina.cz>
2026-04-06 11:33:55 +0200

    Inicializace prisma.ts, hash.ts a [...nextauth].ts

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { prisma } from "@/lib/prisma" 
+import { verifyPassword } from "@/lib/hash"
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+        name: "Credentials",
+
+        credentials: {
+            username: {label: "Uživatelksé jméno", type: "text"},
+            password: {label: "Heslo", type: "password"}
+        },
+
+        async authorize(credentials, req) {
+
+            const user = await prisma.user.findUnique({
+                where: { name: credentials?.username }
+            });
+
+            if (user && await verifyPassword(credentials?.password || "", user.password)){
+                return { id: user.id.toString(), name: user.name };
+            }
+                return null;
+        }
+    })
+  ],
+}
+
+export default NextAuth(authOptions)
